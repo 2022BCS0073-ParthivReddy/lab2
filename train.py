cffd5e715525eb1303f3b06abc67a9025d89bd01
2022BCS0073-ParthivReddy <parthiv22bcs73@iiitkottayam.ac.in>
2026-01-09 22:14:13 +0530

    Model-RandomForest, n_estimators-50, depth-10, testsplit-0.2

--- train.py
+++ train.py
@@ -29,9 +29,6 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 # Model (start with Linear Regression)
-model = LinearRegression()
-model.fit(X_train, y_train)
-
 model = RandomForestRegressor(
     n_estimators=50,
     max_depth=10,
