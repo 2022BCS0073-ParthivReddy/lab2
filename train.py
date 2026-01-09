88a306a213c313bfba4a3d0bd82e32099393749a
2022BCS0073-ParthivReddy <parthiv22bcs73@iiitkottayam.ac.in>
2026-01-09 22:11:03 +0530

    Model-RandomForest, n_estimators-50, depth-10, testsplit-0.2

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import json
 import joblib
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
@@ -30,6 +31,13 @@ X_train, X_test, y_train, y_test = train_test_split(
 # Model (start with Linear Regression)
 model = LinearRegression()
 model.fit(X_train, y_train)
+
+model = RandomForestRegressor(
+    n_estimators=50,
+    max_depth=10,
+    random_state=42
+)
+
 
 # Prediction
 y_pred = model.predict(X_test)
