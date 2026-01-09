5c95f2262de38e8f0a263e46883ea577cdd9ccbe
2022BCS0073-ParthivReddy <parthiv22bcs73@iiitkottayam.ac.in>
2026-01-09 22:25:47 +0530

    Fixed RandomForest fit before prediction in EXP-RF-01

--- train.py
+++ train.py
@@ -25,7 +25,7 @@ y = df["quality"]
 
 # Train-test split
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.3, random_state=42
+    X, y, test_size=0.2, random_state=42
 )
 
 # Model (start with Linear Regression)
@@ -35,6 +35,7 @@ model = RandomForestRegressor(
     random_state=42
 )
 
+model.fit(X_train, y_train)
 
 # Prediction
 y_pred = model.predict(X_test)
